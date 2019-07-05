bbca5840521d308959ea9fe2eccf7e0c4daaf3ce
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-05 03:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class Trip {
     return store.passengers.find(
       function(passenger) {
         return passenger.id === this.passengerId
-      }.bind(this);
+      }.bind(this)
     );
   }
 }
