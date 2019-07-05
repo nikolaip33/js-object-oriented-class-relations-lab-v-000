76775ba3d39eb2363eb4e20279ddc958bb8c7428
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-05 04:34:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,81 @@
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+let store = {drivers: [], passengers: [], trips: [] };
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(
+      function(trip){
+        return trip.driverId === this.id
+      }.bind(this)
+    );
+  }
+
+  passengers() {
+    return this.trips().map(
+      function(trip) {
+        return trip.passenger()
+      }.bind(this)
+    );
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(
+      function(trip){
+        return trip.passengerId === this.id
+      }.bind(this)
+    );
+  }
+
+  drivers() {
+    return this.trips().map(
+      function(trip) {
+          return trip.driver()
+      }.bind(this)
+    );
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+
+    store.trips.push(this);
+  }
+
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId
+      }.bind(this)
+    );
+  }
+
+  driver() {
+    return store.drivers.find(
+      function (driver) {
+        return driver.id === this.driverId
+      }.bind(this)
+    );
+  }
+}
