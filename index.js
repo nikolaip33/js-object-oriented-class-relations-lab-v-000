83b0fd3712330dbcf2490eea2f2e55ab93236b89
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-05 02:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let driverId = 0;
 let passengerId = 0;
+let tripId = 0;
 
 let store = {drivers: [], passengers: [], trips: [] };
 
@@ -22,5 +23,7 @@ class Passenger {
 }
 
 class Trip {
-
+  contructor() {
+    this.id = ++tripId;
+  }
 }
