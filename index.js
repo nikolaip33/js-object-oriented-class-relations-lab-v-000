9b8dbd843d42e71e7db3e7fd2e0f317b72b216f3
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-05 03:00:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ class Trip {
     store.trips.push(this);
   }
   passenger() {
-
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.userId;
+      }.bind(this)
+    );
   }
 }
