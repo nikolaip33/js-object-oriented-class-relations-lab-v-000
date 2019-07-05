4fbcffd7d001c949b05e9ab84c451bfb2d5cbbc1
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-05 02:55:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId;
     this.driverId = driver.id;
-    this.passengerId = passenger;
+    this.passengerId = passenger.id;
 
     store.trips.push(this);
   }
